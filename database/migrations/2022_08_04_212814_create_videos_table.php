30d820eab873349c4d4c9a22f97abc1758b5ead2
marcelodaros94 <root@PE-IT000788.localdomain>
2022-08-04 17:03:32 -0500

    migrations and models

---
+++ database/migrations/2022_08_04_212814_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name','45');
+            $table->string('description','145');
+            $table->text('video');
+            $table->unsignedBigInteger('curso_id');
+            $table->foreign('curso_id')->references('id')->on('cursos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
---
+++ database/migrations/2022_08_04_213600_create_detalle_progresos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetalleProgresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_progresos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('video_num');
+            $table->float('porcentaje');
+            $table->unsignedBigInteger('progreso_id');
+            $table->foreign('progreso_id')->references('id')->on('progresos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalle_progresos');
+    }
+}
