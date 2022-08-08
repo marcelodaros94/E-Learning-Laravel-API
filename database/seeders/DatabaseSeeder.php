38f5437d462180cf6150687e12de8ab00922b718
marcelodaros94 <root@PE-IT000788.localdomain>
2022-08-08 11:15:39 -0500

    Seeders for default content

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Curso;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $curso=new Curso();
+        $curso->name='Psicología del Pro Wrestling'; 
+        $curso->description='Siendo la lucha libre una narrativa más que una simple secuencia; llegamos a algo llamado la suspensión de la incredulidad'; 
+        $curso->img='rock_mankind.webp'; 
+        $curso->price='20.00'; 
+        $curso->video_cant='3';
+        $curso->save();
+
+        $video=new Video();
+        $video->name='Introducción al curso';
+        $video->description='Presentación de los temas a tratar a lo largo del curso';
+        $video->video='intro.mp4';
+        $video->curso_id=$curso->id;
+        $video->save();
+
+        $video2=new Video();
+        $video2->name='Lenguaje corporal';
+        $video2->description='El principal instrumento para contar la historia a través de un combate';
+        $video2->video='body.mp4';
+        $video2->curso_id=$curso->id;
+        $video2->save();
+        
+        $video3=new Video();
+        $video3->name='Generación de emociones';
+        $video3->description='Ejemplificando cómo el desarrollo del personaje permite conectar';
+        $video3->video='emociones.mp4';
+        $video3->curso_id=$curso->id;
+        $video3->save();
+
     }
 }
